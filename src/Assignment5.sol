a012e2c6e3946ba2ddca2b501bb30156624a25f3
M-sharkawy <mostafaeelsharkawy@gmail.com>
2025-03-05 14:27:17 +0200

    solve task

--- src/Assignment5.sol
+++ src/Assignment5.sol
@@ -24,7 +24,7 @@ contract Assignment5 {
     // Initialize `isActive` to true
     constructor() {
         // Fill in the logic
-        isActive = True;
+        isActive = true;
     }
 
     // Function to increment the counter
